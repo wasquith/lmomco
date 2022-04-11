6696e29fb2ec6e3bf5934f16f0ccfccc929999b3
wasquith <william.asquith@ttu.edu>
2022-04-11 09:00:42 -0500

    added trim, leftrim, and rightrim as returned components, though all zero

--- R/lmompdq4.R
+++ R/lmompdq4.R
@@ -1,5 +1,7 @@
 "lmompdq4" <- function(para, paracheck=TRUE) {
-  z <- list(lambdas=rep(NA, 5), ratios=rep(NA, 5), source="lmompdq4")
+  z <- list(lambdas=rep(NA, 5), ratios=rep(NA, 5),
+            trim=0, leftrim=0, rightrim=0,
+            source="lmompdq4")
   if(paracheck == TRUE) {
     if(! are.parpdq4.valid(para)) return()
   }
