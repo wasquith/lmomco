ea11867d327bcbebad2d41ead6ad923f4c33a22d
wasquith <william.asquith@ttu.edu>
2022-04-22 17:51:29 -0500

    added h=NA argument

--- R/pdfpdq4.R
+++ R/pdfpdq4.R
@@ -1,12 +1,12 @@
 "pdfpdq4" <-
-function(x, para, paracheck=TRUE, hfactor=0.2) {
+function(x, para, paracheck=TRUE, h=NA, hfactor=0.2) {
   # Check that the parameters are valid one time
   # then use the paracheck switch on quapdq4 for
   # an extreme speed up on this algorithm.
   if(paracheck == TRUE) {
     if(! are.parpdq4.valid(para)) return()
   }
-  h <- para$para[2] * hfactor
+  if(is.na(h)) h <- para$para[2] * hfactor
   # [-f(x+2h)+8f(x+h)-8f(x-h)+f(x-2h)] / 12h
   f1 <-   -cdfpdq4(x+2*h, para, paracheck=FALSE)
   f2 <-  8*cdfpdq4(x + h, para, paracheck=FALSE)
