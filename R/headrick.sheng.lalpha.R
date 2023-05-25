2da0f57a3b4fdb82188f90ccb4a14cc0d102533b
wasquith <william.asquith@ttu.edu>
2023-05-25 10:21:43 -0500

    additional error-like handling for small samples, no variation in columns

--- R/headrick.sheng.lalpha.R
+++ R/headrick.sheng.lalpha.R
@@ -17,13 +17,13 @@
      zz$text <- "less than two columns in x"
      return(zz)
    }
+   ii <- ""
    for(i in seq_len(pitems)) {
      if(length(unique(x[,i])) == 1) {
-       zz$text <- paste0("no variation in the ", i, "th column, degenerate condition ",
-                         "and L-alpha (or ltm::cronbach.alpha(x)) not finite")
-       alpha <- -Inf; names(alpha) <- "L-alpha"
-       zz$alpha <- alpha
-       return(zz)
+       ii <- ifelse(ii == "", paste0(i, ""), paste0(ii, ",", i))
+     }
+     if(ii != "") {
+       zz$text <- paste0("no variation in column(s) '", ii, "' degenerate condition")
      }
    }
 
@@ -34,9 +34,12 @@
      lco2  <- matrix(nrow=pitems, ncol=pitems)
      for(i in seq_len(pitems)) {
        x[, i+pitems] <- pp(x[,i], sort=FALSE, a=a, ...)
-       lco2[i, i] <- lmoms(x[,i], nmom=2)$lambdas[2]
+       if(length(unique(x[,i])) == 1) {
+         lco2[i, i] <- 0
+       } else {
+         lco2[i, i] <- lmoms(x[,i], nmom=2)$lambdas[2]
+       }
      }
-     #print(x)
      x <- as.matrix(x)
      for(i in seq_len(pitems)) {
        for(j in  seq_len(pitems)) {
@@ -44,11 +47,10 @@
          lco2[i,j] <- 2*stats::cov(x[,i], x[, j+pitems])
        }
      }
-     #print(lco2)
    } else {
      if(is.matrix(x)) {
        lco2 <- x
-       zz$text <- "sample size is unknown, 2nd L-comoments were given"
+       zz$n <- "sample size is unknown, 2nd L-comoments were given"
      } else {
        lco2 <- Lcomoment.matrix(x, k=2)$matrix
        n <- nrow(x); names(n) <- "Sample size"
@@ -56,6 +58,7 @@
      }
    }
 
+   #print(lco2)
 
    dd <- dim(lco2)
    if(dd[1] != dd[2]) {
@@ -70,6 +73,11 @@
         } ))
    } ))
    alpha <- (d / (d-1)) * (1 - A / (A+B))
+   if(alpha > 1) {
+     alpha <- NA
+     zz$text  <- paste0(zz$text, " and\n", "L-alpha computed as > 1, ",
+                        "pathological issues in 'lco2', small sample size?")
+   }
    names(alpha) <- "L-alpha"
    zz$alpha <- round(alpha, digits=digits)
    return(zz)
