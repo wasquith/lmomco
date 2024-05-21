8a3e726c388c429edeecea4b13a6c3b4a5433db0
wasquith <william.asquith@ttu.edu>
2024-05-21 07:22:00 -0500

    further gdd development and testing

--- R/pargdd.R
+++ R/pargdd.R
@@ -1,6 +1,6 @@
 "pargdd" <-
-function(lmom, checklmom=TRUE, symgdd=FALSE, init.para=NULL,
-               silent=FALSE, trace=FALSE, control=list(abstol=0.001, maxit=1000), ...) {
+function(lmom, checklmom=TRUE, symgdd=FALSE, init.para=NULL, snap.tau4=FALSE,
+               silent=FALSE, trace=FALSE, control=list(abstol=0.0001, maxit=1000), ...) {
 
   zz <- list(type="gdd", para=c(NA,NA,NA,NA, as.numeric(symgdd)), source="pargdd")
   if(length(lmom$L1) == 1) { # convert to named L-moments
@@ -9,6 +9,17 @@ function(lmom, checklmom=TRUE, symgdd=FALSE, init.para=NULL,
   if(checklmom & ! are.lmom.valid(lmom)) {
     warning("L-moments are invalid")
     return()
+  }
+
+  message <- ""
+
+  nudge  <- 0.001
+  pe3t3t4 <- lmrdia()$pe3
+  pe3t4   <- approx(pe3t3t4[,1], pe3t3t4[,2], xout=lmom$ratios[3], rule=2)$y
+  if(snap.tau4 & lmom$ratios[4] <= pe3t4) {
+    lmom$ratios[ 4] <- pe3t4 + nudge
+    lmom$lambdas[4] <- lmom$lambdas[2] * lmom$ratios[4]
+    message <- "T4 was below PE3, snapped it to a nudge above, "
   }
 
   L1 <- lmom$lambdas[1]
@@ -29,7 +40,7 @@ function(lmom, checklmom=TRUE, symgdd=FALSE, init.para=NULL,
     }
     if(! are.pargdd.valid(para)) return(Inf); # message(para$para)
     tlmr <- lmomgdd(para, nmom=4, paracheck=FALSE)
-    mu <- (para$para[1]/para$para[2]) - (para$para[3]/para$para[4])
+    mu <- (para$para[1] / para$para[2]) - (para$para[3] / para$para[4])
     if( abs(tlmr$lambdas[1] - mu) > small) return(Inf)
     err <- (tlmr$lambdas[1] - L1)^2 + (tlmr$lambdas[2] - L2)^2 +
            (tlmr$lambdas[3] - L3)^2 + (tlmr$lambdas[4] - L4)^2
@@ -41,38 +52,37 @@ function(lmom, checklmom=TRUE, symgdd=FALSE, init.para=NULL,
     return(sqrt(err))
   }
 
-  integrate(function(f, n=1) { qgamma(f, A1, rate=B1)^n - qgamma(f, A2, rate=B2)^n }, 0, 1, n=1)$value ???
   # E[X^n] = sum(sapply(0:n) function(k) choose(n,k)*(-1)^k*E[X1^(n-k)]*E[X2^k] )
-  mk <- function(n) {
-    txt <- ""
-    for(k in 0:n) {
-      a <- choose(n,k)
-      b <- (-1)^k
-      b <- ifelse(b == -1, "-", "+")
-      c <- paste0("E[X1^", n-k, "]")
-      d <- paste0("E[X2^",   k, "]")
-      txt <- paste0(txt, " ", paste0(b, a,c,d))
-    }
-    print(txt)
-  }
-  pmfunc <- function(par, pm=NA) {
-    para <- exp(par); A1 <- para[1]; B1 <- para[2]; A2 <- para[3]; B2 <- para[4]
-    mugdd <-      A1/B1 - A2/B2  # arithmetic mean
-    sdgdd <- sqrt(A1/B1 + A2/B2) # standard deviation
-    skgdd <- 2*(A1*B2^3 - A2*B1^3) / (A1*B2^2 + A2*B1^2)^(3/2) # revise to 3rd moment, not skew
-    #ktgdd <- ???? # need to derive
-    err <- (mugdd - pm[1])^2 + (sdgdd - pm[2])^2 + (skgdd - pm[3])^2 + (skgdd - pm[3])^2
-    return(err)
-  }
+  #mk <- function(n) {
+  #  txt <- ""
+  #  for(k in 0:n) {
+  #    a <- choose(n,k)
+  #    b <- (-1)^k
+  #    b <- ifelse(b == -1, "-", "+")
+  #    c <- paste0("E[X1^", n-k, "]")
+  #    d <- paste0("E[X2^",   k, "]")
+  #    txt <- paste0(txt, " ", paste0(b, a,c,d))
+  #  }
+  #  print(txt)
+  #}
 
   if(is.null(init.para)) {
-    #pm <- pmoms(init.para, checklmom=FALSE) # when testing starts, init.para sneak way to get x into function
-    #rt <- NULL
-    #try(rt <- optim(c(0,0,0,0), pmfunc, pm=pm))
-    #print(rt)
-    #init.para <- exp(rt$par)
-    #stop()
-    init.para <- c(10, 3, 2, 0.2)
+    F1 <- runif(5000); F2 <- runif(5000)
+    mcfunc <- function(par, lmr=NA) {
+      p <- exp(par); if(symgdd) p <- c(p[1:2], p[1:2])
+      tlmr <- pwm2lmom( pwm( qgamma(F1, p[1], p[2]) -  qgamma(F2, p[3], p[4]) ) )
+      if(! are.lmom.valid(tlmr)) return(Inf)
+      pe3t4 <- approx(pe3t3t4[,1], pe3t3t4[,2], xout=tlmr$ratios[3], rule=2)$y
+      if(tlmr$ratios[4] <= pe3t4) return(Inf) # below the Pearson III
+      sum((lmr$lambdas[1:4] - tlmr$lambdas[1:4])^2)
+    }
+    try(sara <- optim(c(0, 0, 0, 0), mcfunc, lmr=lmom), silent=FALSE )
+    if(is.null(sara)) {
+      init.para <- c(10, 3, 2, 0.2)
+    } else {
+      init.para <- exp(sara$par)
+      message <- paste0(message, "initial parameters from Monte Carlo qgamma()x2")
+    }
     if(symgdd) init.para <- init.para[c(1:2, 1:2)]
   } else {
     if(length(init.para) == 5) {
@@ -95,8 +105,9 @@ function(lmom, checklmom=TRUE, symgdd=FALSE, init.para=NULL,
     para[4] <- para[2]
     para[5] <- 1
   }
-  zz$para <- para
-  zz$optim <- rt
+  zz$para      <- para
+  zz$message   <- message
+  zz$optim     <- rt
+  zz$init.para <- exp(init.para)
   return(zz)
 }
-
