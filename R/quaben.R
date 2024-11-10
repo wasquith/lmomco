3f221a792b08811483b5eed42cad4797250b4b63
wasquith <william.asquith@ttu.edu>
2024-11-10 09:14:29 -0600

    updates to the Benford subsystems

--- R/quaben.R
+++ R/quaben.R
@@ -1,8 +1,11 @@
 "quaben" <- function(f, para=list(para=c(1, 10)), ...) {
   m <- para$para[1]
+  if(m > 9) {
+    warning("expect failure as first parameter is an integer greater than 9")
+  }
   b <- para$para[2]
   if(b != 10) {
-    warning("pmfben is only configured for base10 at the moment")
+    warning("pmfben is only configured for base10 at the moment in the second parameter")
     return(NULL)
   }
   s <- (1*b^(m-1))
